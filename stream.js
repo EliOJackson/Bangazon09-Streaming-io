7acd23ecd15a0c0e95b66c15c737eb1e2203f064
Eli Jackson <tucker.eli.jackson@gmail.com>
2018-03-07 21:59:50 -0600

    assignment complete, able to write to json

--- stream.js
+++ stream.js
@@ -6,18 +6,18 @@ const { Transform, Writable } = require('stream');
 const upperCase = Transform();
 const writeStream = Writable();
 
-upperCase._transform = (buffer, _, cb) => {
-    cb(null, buffer.toString().toUpperCase());
+upperCase._transform = (buffer, _, callback) => {
+    callback(null, buffer.toString().toUpperCase());
 }
 
 writeStream._write = (buffer, _, next) => {
     writeFile(fileArg, buffer, (err) => {
         if (err) throw err;
-        process.stdout.write('The data has been added\n');
+        console.log("Data Added!")
     });
     next();
 };
 
 createReadStream("languages.json")
-    .pipe(upperCase)
-    .pipe(writeStream);
+.pipe(upperCase)
+.pipe(writeStream);
